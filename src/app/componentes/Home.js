50764328c7aee70ff2f5b5e376332a80aea1401c
valencsoares <pvcampossoares558@gmail.com>
2024-07-23 21:30:51 -0400

    Feito a barra de pesquisa, na qual quando está nula, retorna toda as pelucias ja salvas em ambas as listas, e conforme escreve, é filtrado na lista nova para que retorne apenas os produtos que estejam de acordo com a pesquisa. Foi feito a estilização também

--- src/app/componentes/Home.js
+++ src/app/componentes/Home.js
@@ -10,6 +10,8 @@ import Link from "next/link";
 export default function Home() {
     const [deuErro, setDeuErro] = useState(false)
     const [listaPelucias, setListaPelucias] = useState([])
+    const [outraListaPelucias, setOutraListaPelucias] = useState([])
+    const [pesquisaBarra, setPesquisaBarra] = useState("")
 
     useEffect(() => {
         const pegarPelucia = async () => {
@@ -17,6 +19,7 @@ export default function Home() {
                 const resposta = await fetch("http://localhost:3000/api", { next: { revalidate: 1 } });
                 const pelucias = await resposta.json();
                 setListaPelucias(pelucias)
+                setOutraListaPelucias(pelucias)
             } catch {
                 setDeuErro(true)
             }
@@ -45,25 +48,44 @@ export default function Home() {
         setListaPelucias(listaAux)
     }
 
+    const pesquisa = (texto) => {
+        setPesquisaBarra(texto)
 
-    if (listaPelucias[0] == null){
-        return <Carregar/>
+        if(texto == ""){
+            setListaPelucias(outraListaPelucias)
+            return
+        }
+        const novaLista = listaPelucias.filter((pelucia) => 
+            pelucia.titulo.toUpperCase().trim().includes(pesquisaBarra.toUpperCase().trim())
+        )
+        setListaPelucias(novaLista)
+    }
+
+    if (deuErro == true) {
+        return <ErroNoFetch />
+    }
+
+    if (outraListaPelucias[0] == null) {
+        return <Carregar />
     }
 
     return (
         <main>
             <div className={styles.input}>
+                <input className={styles.pesquisar} type="text" value={pesquisaBarra} placeholder="Pesquise um produto" onChange={(event) => pesquisa(event.target.value)}/>
                 <button className={styles.botao3} onClick={ordenaAz}> Organize pelo título de A a Z </button>
                 <button className={styles.botao3} onClick={ordenaZa}> Organize pelo título de Z a A </button>
                 <button className={styles.botao3} onClick={ordenaFranquiaAz}> Organize pela franquia </button>
                 <button className={styles.botao3} onClick={precoCrescente}> Filtar por menor preço </button>
                 <button className={styles.botao3} onClick={precoDecrescente}> Filtar por maior preço </button>
+            </div>
             <div className={styles.container}>
                 {listaPelucias.map((p) => (
                     <div className={styles.card}>
                         <Image width={220} height={250} src={p.imagem} />
                         <p className={styles.textoTitulo}>{p.titulo}</p>
                         <p className={styles.texto}>{p.franquia}</p>
+                        <p className={styles.texto}>R${p.preco},00</p>
                         <Link href={"/pelucia/" + p.id}>
                             <div>
                                 <button className={styles.botao}>Ver mais</button>
